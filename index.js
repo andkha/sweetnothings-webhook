2a17f8b4ab4514ff76f2ddcaa038a91ce306d8cf
andkha <andkha625@gmail.com>
2019-12-11 10:13:02 +1000

    change service to SG

--- index.js
+++ index.js
@@ -2,20 +2,36 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 
-const mailgun = require("mailgun-js");
-const DOMAIN = "sandboxd0aa85e296194df6bbe7118617c03032.mailgun.org";
-const mg = mailgun({
-  apiKey: process.env.API_KEY,
-  domain: DOMAIN
-});
+// const mailgun = require("mailgun-js");
+// const DOMAIN = "sandboxd0aa85e296194df6bbe7118617c03032.mailgun.org";
+// const mg = mailgun({
+//   apiKey: process.env.API_KEY,
+//   domain: DOMAIN
+// });
 
-const data = ({ email, ...variables }) => ({
-  from:
-    "Mailgun Sandbox <postmaster@sandboxd0aa85e296194df6bbe7118617c03032.mailgun.org>",
-  to: email,
+// const data = ({ email, ...variables }) => ({
+//   from:
+//     "Mailgun Sandbox <postmaster@sandboxd0aa85e296194df6bbe7118617c03032.mailgun.org>",
+//   to: email,
+//   subject: "A sweet gift is on the way!",
+//   template: "gift-wrapping",
+//   "h:X-Mailgun-Variables": JSON.stringify(variables)
+// });
+
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const data = (
+  { email, ...variables } = {
+    gift_note: "Gift message from Sweet Nothings",
+    date: `${new Date().getMonth() +
+      1}/${new Date().getDate()}/${new Date().getFullYear()}`
+  }
+) => ({
+  to: email || "andkha625@gmail.com",
+  from: "hello@eatsweetnothings.com",
   subject: "A sweet gift is on the way!",
-  template: "gift-wrapping",
-  "h:X-Mailgun-Variables": JSON.stringify(variables)
+  templateId: "d-025cac0728104fe78aedaccf7d2e34b7",
+  dynamic_template_data: variables
 });
 
 const express = require("express");
@@ -49,9 +65,13 @@ app.post("/:type", (req, res) => {
     console.log(forLog, config);
 
     if (config.email) {
-      mg.messages().send(data(config), function(error, body) {
-        console.log(body);
-      });
+      // mg.messages().send(data(config), function(error, body) {
+      //   console.log(body);
+      // });
+      sgMail
+        .send(data(config))
+        .then(console.log)
+        .catch(console.error);
     }
   }
   return res.send({ message: "Thanks" });
